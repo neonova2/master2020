76aa15e8b6ce529e8025de646f69becbb952f330
neonova2 <77122036+neonova2@users.noreply.github.com>
2021-01-08 09:04:49 +0530

    Update qtbutton.cpp

--- OST/exp10-gui/qtbutton.cpp
+++ OST/exp10-gui/qtbutton.cpp
@@ -1,16 +1,18 @@
 #include <QApplication>
 #include <QPushButton>
+#include <QVBoxLayout>
+#include <QTextEdit>
 int main(int argc, char *argv[])
 {
 	QApplication app(argc, argv);
-	QTextEdit *textEdit = new QTextEdit;
+	QTextEdit *textEdit = new QTextEdit();
 	QPushButton *quitbutton = new QPushButton("&Quit");
-	QObject::connect(quitButton, SIGNAL(clicked()), qApp, SLOT(quit()));
-	QVBoxLayout *layout = new QVBoxLayout;
+	QObject::connect(quitbutton, SIGNAL(clicked()), qApp, SLOT(quit()));
+	QVBoxLayout *layout = new QVBoxLayout();
 	layout->addWidget(textEdit);
-	layout-addWidget(quitButton);
+	layout->addWidget(quitbutton);
 	QWidget window;
-	window.setlayout(layout);
+	window.setLayout(layout);
 	window.show();
 	return app.exec();
 }
